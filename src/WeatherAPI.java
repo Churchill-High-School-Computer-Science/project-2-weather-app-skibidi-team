709bebc93899a41c11bb19bee846664e656c802c
Russell Poteet <rwpoteet@icloud.com>
2024-09-18 13:23:58 -0500

    submit

--- src/WeatherAPI.java
+++ src/WeatherAPI.java
@@ -48,7 +48,7 @@ public class WeatherAPI {
     // Function to get, format, and return weather data
     public String parseWeatherData(String blockOfText) {
 
-        return "Weather in " + getCityName(blockOfText) + "\nTemperature: "  + getTemp(blockOfText) + "\nHumidity: " + getHumidity(blockOfText);
+        return "Weather in " + getCityName(blockOfText) + "\nTemperature: "  + getTemp(blockOfText) + "F\nHumidity: " + getHumidity(blockOfText) + "%\nWind Speed: " + getWindSpeed(blockOfText)+ "mph";
 
     }
 
